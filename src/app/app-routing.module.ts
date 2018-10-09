b3f4b2cf591c4e94fb31990c6e4e25d6c8349c68
v8vikas <v8.vikas@gmail.com>
2018-10-10 02:14:27 +0530

    - Added routing. - Fixed to max things common.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Routes, RouterModule} from '@angular/router';
 import { LoginComponent } from './layout/auth/login/login.component';
+import { RegisterComponent } from './layout/auth/register/register.component';
 
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', redirectTo: '/login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 
   { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
