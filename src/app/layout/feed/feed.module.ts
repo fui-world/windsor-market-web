03e8c222eb05d4732b16e38fae22d9330e6575df
FUi <fuiworldsolution@gmail.com>
2018-10-12 03:40:56 +0530

    - Created start feed page - Add login button on start page. - Worked on reponsive part.

--- src/app/layout/feed/feed.module.ts
+++ src/app/layout/feed/feed.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StartComponent } from './start/start.component';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../material/material.module';
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MaterialModule
   ],
   declarations: [StartComponent],
   exports: [StartComponent]
